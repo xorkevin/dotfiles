c7b01f92d289ca35d8b77d3b1caf6db0daacf031
Kevin Wang <kevin@xorkevin.com>
2019-10-02 19:10:05 -0700

    Remove org-evil

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,6 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -17,27 +16,15 @@
 
 (use-package org
              :ensure t)
-
-(use-package dash
-             :ensure t)
-
-(use-package monitor
-             :ensure t)
-
-(use-package org-evil
-             :ensure t
-             :after (evil org dash monitor))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil-visual-mark-mode evil))))
+ '(org-agenda-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
