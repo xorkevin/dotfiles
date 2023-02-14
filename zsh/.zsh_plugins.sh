ceee9beb6f7decf82e0137e128c27a9fed318975
Kevin Wang <kevin@xorkevin.com>
2023-02-14 00:21:15 -0800

    Remove unused zsh plugins

--- zsh/.zsh_plugins.sh
+++ zsh/.zsh_plugins.sh
@@ -1,6 +1,2 @@
 source /home/kevin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
 fpath+=( /home/kevin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/kevin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-xorkevin-SLASH-code-review-zsh/code-review.plugin.zsh
-fpath+=( /home/kevin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-xorkevin-SLASH-code-review-zsh )
-source /home/kevin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-xorkevin-SLASH-launchcpx/flightctl.plugin.zsh
-fpath+=( /home/kevin/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-xorkevin-SLASH-launchcpx )
