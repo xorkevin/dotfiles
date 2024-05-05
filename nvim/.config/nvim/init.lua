eb1791e7e382fdd24f23751bfc65906ba8e810d9
Kevin Wang <kevin@xorkevin.com>
2024-05-04 18:05:00 -0700

    Disable cmp-path

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -761,7 +761,7 @@ require('lazy').setup({
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+      -- 'hrsh7th/cmp-path'
       'hrsh7th/cmp-buffer',
     },
     config = function()
