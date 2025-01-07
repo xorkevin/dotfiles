c99cb258183d5ba15d23a55e2709da1f5df92c66
Kevin Wang <kevin@xorkevin.com>
2025-01-07 15:53:36 +0800

    Change headphone outputs

--- scripts/scripts/audio/tospeakers.sh
+++ scripts/scripts/audio/tospeakers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sinkh=$(pactl list short sinks | cut -f2 | grep 'FiiO')
-sinks=$(pactl list short sinks | cut -f2 | grep 'Vanatoo')
+sinkh=$(pactl list short sinks | cut -f2 | grep 'bluez')
+sinks=$(pactl list short sinks | cut -f2 | grep 'analog-stereo')
 if [ ! -z "$sinkh" ]; then
   pactl set-sink-mute "$sinkh" true
 fi
