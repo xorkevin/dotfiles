fa5160bfd88b14d31c670c57916e0b374dedeca3
Kevin Wang <kevin@xorkevin.com>
2025-08-23 22:32:50 -0700

    Impl lock screen resize

--- scripts/scripts/lockscreen.sh
+++ scripts/scripts/lockscreen.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-i3lock -i "$HOME/Pictures/lock"
+screendim="3840x2160"
+
+if command -v convert > /dev/null && [ -e "$HOME/Pictures/lock" ]; then
+  convert "$HOME/Pictures/lock" -resize "${screendim}^" \
+    -gravity Center -extent "$screendim" RGB:- \
+    | i3lock --raw "${screendim}:rgb" --image /dev/stdin
+else
+  i3lock --color 000000
+fi
